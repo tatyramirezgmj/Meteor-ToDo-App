351cab299315c13342e93628c607ee02f0fc932d
tatyramirezgmj <tatiana@trwdesignsolutions.com>
2017-07-11 19:07:50 -0400

    To-do list now functional

---
+++ imports/ui/AccountsUIWrapper.jsx
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import { Template } from 'meteor/templating';
+import { Blaze } from 'meteor/blaze';
+
+export  default class AccountsUIWrapper extends Component {
+  componentDidMount(){
+    //use Meteo Blaze to render login buttons
+    this.view = Blaze.render(Template.loginButtons,
+      ReactDOM.findDOMNode(this.refs.container));
+  }
+
+  componentWillUnmount() {
+    //clean up Blaze view
+    Blaze.remove(this.view);
+  }
+  render() {
+    //Just render a placeholder container that will be filled in
+    return <span ref="container" />;
+  }
+}
